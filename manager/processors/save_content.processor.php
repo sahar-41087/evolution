bde06b00f14373a3a62d60f767ce59eeaa694828
Александр Левченко <seo.supra87@mail.ru>
2025-07-01 11:06:21 +0300

    Update save_content.processor.php
    add createdon

--- manager/processors/save_content.processor.php
+++ manager/processors/save_content.processor.php
@@ -54,7 +54,7 @@ $hide_from_tree = (int) $_POST['hide_from_tree'];
 $menutitle = $_POST['menutitle'];
 $hidemenu = (int) $_POST['hidemenu'];
 $aliasvisible = (int) $_POST['alias_visible'];
-$createdon = (int) $_POST['createdon'] ?? strtotime('now');
+$createdon = strtotime($_POST['createdon']) ?? strtotime('now');
 /************* webber ********/
 $sd = isset($_POST['dir']) && strtolower($_POST['dir']) === 'asc' ? '&dir=ASC' : '&dir=DESC';
 $sb = isset($_POST['sort']) ? '&sort=' . e($_POST['sort']) : '&sort=pub_date';
